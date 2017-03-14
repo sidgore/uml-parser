3adf1a4ed26324ef50611b84ae48d2d35a80d796
SIDDHARTH GORE <sidgore@SIDDHARTHs-MacBook-Air.local>
2017-03-13 18:19:40 -0700

    Commit

--- src/JavaToString.java
+++ src/JavaToString.java
@@ -9,17 +9,40 @@ import java.util.HashMap;
 public class JavaToString {
 	   final String input;
 	    final String output;
-	    String code;
+	    String code,os;
 
 	    JavaToString(String input, String output) {
+	    	os=System.getProperty("os.name");
+	    
 	        this.input = input;
 	        this.output = input + "/" + output + ".png";
 	      
 	         code = "";
 	    }
 	    public void start()  {
+	    	File dir = new File(input);
+	    	int count=0;
+	    	String[] files = dir.list();
+	    
 	    	code="[Customer|-forname:string;surname:string|doShiz()]<>-orders*>[Order] [Order]++-0..*>[LineItem] [Order]-[note:Aggregate root{bg:wheat}]";
 	        System.out.println("Input Path: " + input);
+	        if(files.length > 0){				
+				System.out.println("Java file parsing in progress...");
+				for(int i=0; i< files.length;i++)
+				{
+					if(files[i].endsWith(".java")){
+						System.out.println(files[i].toString());
+						//if("Windows".equalsIgnoreCase(os))
+							count++;
+							
+						
+						}
+						
+					System.out.println(count);
+					}			
+				}
+	        
+	        
 	        System.out.println("Unique Code: " + code);
 	        System.out.println("Out path: " + output);
 	     
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 public static void main(String[] args) {
 	//String test="";
 	
-	JavaToString obj = new JavaToString("", "diagram");
+	JavaToString obj = new JavaToString("/Users/sidgore/Downloads/downloads/paul 202 github/umlparser/uml-parser-test-1", "diagram");
    obj.start();
 	System.out.println("Hello World");
 	
