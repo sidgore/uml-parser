0557b40d38b1a770a5481698a94fb8736c3be073
SIDDHARTH GORE <sidgore@SIDDHARTHs-MacBook-Air.local>
2017-04-06 03:43:41 -0700

    Sid Commit

--- src/JavaToString.java
+++ src/JavaToString.java
@@ -1,20 +1,24 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
 
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ast.CompilationUnit;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+import com.github.javaparser.*;
+import com.github.javaparser.ast.*;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
+
 
 
 
 
 
 public class JavaToString {
-	LinkedList<CompilationUnit> linkedList = new LinkedList();
+	LinkedList<CompilationUnit> linkedList = new LinkedList<CompilationUnit>();
 	   final String input;
 	    final String output;
 	    String code;
+	    HashMap<String, Boolean> map;
 
 	    JavaToString(String input, String output) {
 	    	
@@ -24,12 +28,12 @@ public class JavaToString {
 	      
 	         code = "";
 	    }
-	    public void start() throws Exception  {
+	    public void start()   {
 	    	File dir = new File(input);
 	    	int count=0;
 	    	File[] files = dir.listFiles();
-	    	CompilationUnit cu ;
-	    	FileInputStream in;
+	    	CompilationUnit cu = null ;
+	    	FileInputStream in = null;
 	    	//code="[Customer|-forname:string;surname:string|doShiz()]<>-orders*>[Order] [Order]++-0..*>[LineItem] [Order]-[note:Aggregate root{bg:wheat}]";
 	        System.out.println("Input Path: " + input);
 	        if(files.length > 0){				
@@ -39,9 +43,24 @@ public class JavaToString {
 					
 					if(files[i].getName().endsWith(".java"))
 					{
-						 in = new FileInputStream(files[i]);
-						  cu = JavaParser.parse(in);
-							in.close();
+						 try {
+							in = new FileInputStream(files[i]);
+						} catch (FileNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						  try {
+							cu = JavaParser.parse(in);
+						} catch (ParseException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+							try {
+								in.close();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 						  
 						  linkedList.add(cu);
 						// cuArray.add(cu);
@@ -58,6 +77,14 @@ public class JavaToString {
 					}
 				System.out.println(count);
 				}
+	        for (CompilationUnit c : linkedList) {
+	            List<TypeDeclaration> cl = c.getTypes();
+	            for (Node n : cl) {
+	                ClassOrInterfaceDeclaration coi = (ClassOrInterfaceDeclaration) n;
+	                map.put(coi.getName(), coi.isInterface()); 
+	              
+	            }
+	        }
 	        
 	        
 	        System.out.println("Unique Code: " + code);
