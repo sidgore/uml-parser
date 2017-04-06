12e2efd1d32a82eadfa9c7feb05af352c8f50feb
SIDDHARTH GORE <sidgore@SIDDHARTHs-MacBook-Air.local>
2017-04-06 03:11:50 -0700

    Sid Commit

--- src/JavaToString.java
+++ src/JavaToString.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
@@ -10,12 +11,13 @@ import com.github.javaparser.ast.CompilationUnit;
 
 
 public class JavaToString {
+	LinkedList<CompilationUnit> linkedList = new LinkedList();
 	   final String input;
 	    final String output;
-	    String code,os;
+	    String code;
 
 	    JavaToString(String input, String output) {
-	    	os=System.getProperty("os.name");
+	    	
 	    
 	        this.input = input;
 	        this.output = input + "/" + output + ".png";
@@ -26,25 +28,31 @@ public class JavaToString {
 	    	File dir = new File(input);
 	    	int count=0;
 	    	File[] files = dir.listFiles();
-	    
-	    	code="[Customer|-forname:string;surname:string|doShiz()]<>-orders*>[Order] [Order]++-0..*>[LineItem] [Order]-[note:Aggregate root{bg:wheat}]";
+	    	CompilationUnit cu ;
+	    	FileInputStream in;
+	    	//code="[Customer|-forname:string;surname:string|doShiz()]<>-orders*>[Order] [Order]++-0..*>[LineItem] [Order]-[note:Aggregate root{bg:wheat}]";
 	        System.out.println("Input Path: " + input);
 	        if(files.length > 0){				
 				System.out.println("Java file parsing in progress...");
 				for(int i=0; i< files.length;i++)
 				{
+					
 					if(files[i].getName().endsWith(".java"))
 					{
-						FileInputStream in = new FileInputStream(files[i]);
-						 CompilationUnit cu = JavaParser.parse(in);
+						 in = new FileInputStream(files[i]);
+						  cu = JavaParser.parse(in);
+							in.close();
+						  
+						  linkedList.add(cu);
 						// cuArray.add(cu);
-						 System.out.println(cu);
+						// System.out.println(cu);
 						//System.out.println(files[i].toString());
-						//if("Windows".equalsIgnoreCase(os))
+						
 							count++;
 							System.out.println(count);
 						
 						}
+				
 						
 					//System.out.println(count);
 					}
