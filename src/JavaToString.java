fa1ec11d330fda0990d1b4f57a7ed3010e381d5e
SIDDHARTH GORE <sidgore@sid.local>
2017-04-15 19:32:11 -0700

    Sid Commit

--- src/JavaToString.java
+++ src/JavaToString.java
@@ -5,8 +5,14 @@ import java.lang.*;
 
 import com.github.javaparser.*;
 import com.github.javaparser.ast.*;
+
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.TypeDeclaration;
+
+
+
+import com.github.javaparser.ast.body.*;
+
 
 
 
@@ -19,10 +25,12 @@ public class JavaToString {
 	    final String output;
 	    String code;
 	    HashMap<String, Boolean> map;
+	    HashMap<String, String> mapClassConn;
 
 	    JavaToString(String input, String output) {
 	    	 map = new HashMap<String, Boolean>();
-	    
+	         mapClassConn = new HashMap<String, String>();
+	        
 	        this.input = input;
 	        this.output = input + "/" + output + ".png";
 	      
@@ -51,6 +59,7 @@ public class JavaToString {
 						}
 						  try {
 							cu = JavaParser.parse(in);
+							
 						} catch (ParseException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
@@ -87,15 +96,225 @@ public class JavaToString {
 	                map.put(coi.getName(), coi.isInterface()); 
 	              
 	            }}
+	        
+	        //add class names to code!!
+	        
+	        for (CompilationUnit c : linkedList)
+	        {
+	        	String result = "";
+	            String className = "";
+	            String classShortName = "";
+	            String methods = "";
+	            String fields = "";
+	            String additions = ",";
+	            
+	            ArrayList<String> makeFieldPublic = new ArrayList<String>();
+	            List<TypeDeclaration> ltd = c.getTypes();
+	            Node node = ltd.get(0); // assuming no nested classes
+	            
+	         // Get className
+	           // for (Node n1 : ltd) {
+	            ClassOrInterfaceDeclaration coii = (ClassOrInterfaceDeclaration) node;
+	           
+	            if (coii.isInterface()) {
+	                className = "[" + "<<interface>>;";
+	            } else {
+	                className = "[";
+	            }
+	            className += coii.getName();
+	            classShortName = coii.getName();
+	            
+	         code += className;
+	        
+	        
+	     // Parsing Methods
+	        boolean nextParam = false;
+	        for (BodyDeclaration bd : ((TypeDeclaration) node).getMembers()) {
+	            // Get Methods
+	            if (bd instanceof ConstructorDeclaration) {
+	                ConstructorDeclaration cd = ((ConstructorDeclaration) bd);
+	                if (cd.getDeclarationAsString().startsWith("public")
+	                        && !coii.isInterface()) {
+	                    if (nextParam)
+	                        methods += ";";
+	                    methods += "+ " + cd.getName() + "(";
+	                    for (Object gcn : cd.getChildrenNodes()) {
+	                        if (gcn instanceof Parameter) {
+	                            Parameter paramCast = (Parameter) gcn;
+	                            String paramClass = paramCast.getType().toString();
+	                            String paramName = paramCast.getChildrenNodes()
+	                                    .get(0).toString();
+	                            methods += paramName + " : " + paramClass;
+	                            if (map.containsKey(paramClass)
+	                                    && !map.get(classShortName)) {
+	                                additions += "[" + classShortName
+	                                        + "] uses -.->";
+	                                if (map.get(paramClass))
+	                                    additions += "[<<interface>>;" + paramClass
+	                                            + "]";
+	                                else
+	                                    additions += "[" + paramClass + "]";
+	                            }
+	                            additions += ",";
+	                        }
+	                    }
+	                    methods += ")";
+	                    nextParam = true;
+	                }
+	            }
+	        }
+	        for (BodyDeclaration bd : ((TypeDeclaration) node).getMembers()) {
+	            if (bd instanceof MethodDeclaration) {
+	                MethodDeclaration md = ((MethodDeclaration) bd);
+	                // Get only public methods
+	                if (md.getDeclarationAsString().startsWith("public")
+	                        && !coii.isInterface()) {
+	                    // Identify Setters and Getters
+	                    if (md.getName().startsWith("set")
+	                            || md.getName().startsWith("get")) {
+	                        String varName = md.getName().substring(3);
+	                        makeFieldPublic.add(varName.toLowerCase());
+	                    } else {
+	                        if (nextParam)
+	                            methods += ";";
+	                        methods += "+ " + md.getName() + "(";
+	                        for (Object gcn : md.getChildrenNodes()) {
+	                            if (gcn instanceof Parameter) {
+	                                Parameter paramCast = (Parameter) gcn;
+	                                String paramClass = paramCast.getType()
+	                                        .toString();
+	                                String paramName = paramCast.getChildrenNodes()
+	                                        .get(0).toString();
+	                                methods += paramName + " : " + paramClass;
+	                                if (map.containsKey(paramClass)
+	                                        && !map.get(classShortName)) {
+	                                    additions += "[" + classShortName
+	                                            + "] uses -.->";
+	                                    if (map.get(paramClass))
+	                                        additions += "[<<interface>>;"
+	                                                + paramClass + "]";
+	                                    else
+	                                        additions += "[" + paramClass + "]";
+	                                }
+	                                additions += ",";
+	                            } else {
+	                                String methodBody[] = gcn.toString().split(" ");
+	                                for (String foo : methodBody) {
+	                                    if (map.containsKey(foo)
+	                                            && !map.get(classShortName)) {
+	                                        additions += "[" + classShortName
+	                                                + "] uses -.->";
+	                                        if (map.get(foo))
+	                                            additions += "[<<interface>>;" + foo
+	                                                    + "]";
+	                                        else
+	                                            additions += "[" + foo + "]";
+	                                        additions += ",";
+	                                    }
+	                                }
+	                            }
+	                        }
+	                        methods += ") : " + md.getType();
+	                        nextParam = true;
+	                    }
+	                }
+	            }
+	        }
+	        
+	        
+	        
+	        // Parsing Fields
+	        boolean nextField = false;
+	        for (BodyDeclaration bd : ((TypeDeclaration) node).getMembers()) {
+	            if (bd instanceof FieldDeclaration) {
+	                FieldDeclaration fd = ((FieldDeclaration) bd);
+	                String fieldScope = aToSymScope(
+	                        bd.toStringWithoutComments().substring(0,
+	                                bd.toStringWithoutComments().indexOf(" ")));
+	                String fieldClass = changeBrackets(fd.getType().toString());
+	                String fieldName = fd.getChildrenNodes().get(1).toString();
+	                if (fieldName.contains("="))
+	                    fieldName = fd.getChildrenNodes().get(1).toString()
+	                            .substring(0, fd.getChildrenNodes().get(1)
+	                                    .toString().indexOf("=") - 1);
+	                // Change scope of getter, setters
+	                if (fieldScope.equals("-")
+	                        && makeFieldPublic.contains(fieldName.toLowerCase())) {
+	                    fieldScope = "+";
+	                }
+	                String getDepen = "";
+	                boolean getDepenMultiple = false;
+	                if (fieldClass.contains("(")) {
+	                    getDepen = fieldClass.substring(fieldClass.indexOf("(") + 1,
+	                            fieldClass.indexOf(")"));
+	                    getDepenMultiple = true;
+	                } else if (map.containsKey(fieldClass)) {
+	                    getDepen = fieldClass;
+	                }
+	                if (getDepen.length() > 0 && map.containsKey(getDepen)) {
+	                    String connection = "-";
+
+	                    if (mapClassConn
+	                            .containsKey(getDepen + "-" + classShortName)) {
+	                        connection = mapClassConn
+	                                .get(getDepen + "-" + classShortName);
+	                        if (getDepenMultiple)
+	                            connection = "*" + connection;
+	                        mapClassConn.put(getDepen + "-" + classShortName,
+	                                connection);
+	                    } else {
+	                        if (getDepenMultiple)
+	                            connection += "*";
+	                        mapClassConn.put(classShortName + "-" + getDepen,
+	                                connection);
+	                    }
+	                }
+	                if (fieldScope == "+" || fieldScope == "-") {
+	                    if (nextField)
+	                        fields += "; ";
+	                    fields += fieldScope + " " + fieldName + " : " + fieldClass;
+	                    nextField = true;
+	                }
+	            }
+
+	        }
+	        
+	        
+	        
+	        
+	
+	        }
+	        
+	   
+	        
 	        for (String key : map.keySet()) {
 	            System.out.println(key + " " + map.get(key));
 	        }
 	        
 	        
-	       // System.out.println("Unique Code: " + code);
+	        System.out.println("Unique Code: " + code);
 	        //System.out.println("Out path: " + output);
+	        
 	     
 	    }
+	     private String aToSymScope(String stringScope) {
+	            switch (stringScope) {
+	            case "private":
+	                return "-";
+	            case "public":
+	                return "+";
+	            default:
+	                return "";
+	            }
+	        }
+	     private String changeBrackets(String foo) {
+	            foo = foo.replace("[", "(");
+	            foo = foo.replace("]", ")");
+	            foo = foo.replace("<", "(");
+	            foo = foo.replace(">", ")");
+	            return foo;
+	        }
+	        
 	    
 	    
 }
