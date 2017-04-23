2866c72067bc22d1a8cf575e1afe44a216502969
SIDDHARTH GORE <sidgore@sid.local>
2017-04-22 20:39:46 -0700

    sid commit

--- src/SequenceDiagram.java
+++ src/SequenceDiagram.java
@@ -1,22 +1,10 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ParseException;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.Node;
-import com.github.javaparser.ast.body.BodyDeclaration;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.ConstructorDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.body.Parameter;
-import com.github.javaparser.ast.body.TypeDeclaration;
+
 import com.github.javaparser.ast.expr.MethodCallExpr;
 
 public class SequenceDiagram {
@@ -31,26 +19,30 @@ public class SequenceDiagram {
 	HashMap<String, String> Connect;
 	HashMap<String, ArrayList<MethodCallExpr>> functionCalls;
 
-	
-    ArrayList<CompilationUnit> compilationUnit;
-    HashMap<String, ArrayList<MethodCallExpr>> functionCall;
+	ArrayList<CompilationUnit> compilationUnit;
+	HashMap<String, ArrayList<MethodCallExpr>> functionCall;
 
-	SequenceDiagram(String input, String output,String ClassName,String FunctionName) {
-		
-		
-		
-	
-        this.input = input;
-        this.output = input + "\\" + output + ".png";
-        this.ClassName = ClassName;
-        this.FunctionName = FunctionName;
-        map = new HashMap<String, String>();
-        functionCalls = new HashMap<String, ArrayList<MethodCallExpr>>();
-        code = "@startuml\n";
+	SequenceDiagram(String input, String output, String ClassName, String FunctionName) {
+
+		this.input = input;
+		this.output = input + "\\" + output + ".png";
+		this.ClassName = ClassName;
+		this.FunctionName = FunctionName;
+		map = new HashMap<String, String>();
+		functionCalls = new HashMap<String, ArrayList<MethodCallExpr>>();
+		code = "@startuml\n";
 	}
-	
-	
-	
+
 	public void execute() {
+
+		code += "actor user #black\n";
+		code += "user" + " -> " + input + " : " + FunctionName + "\n";
+		code += "activate ";
+
+		code += "@enduml";
+
+		System.out.println("Plant UML Code:\n" + code);
+
 	}
+
 }
