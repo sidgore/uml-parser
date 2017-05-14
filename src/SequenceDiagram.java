ccc0b3fa23c189f7d689fe09ba9eeaebb78c8d9b
SIDDHARTH GORE <sidgore@sid.local>
2017-05-13 21:57:59 -0700

    sid commit

--- src/SequenceDiagram.java
+++ src/SequenceDiagram.java
@@ -1,127 +1,99 @@
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
-
-
-
-
-
-
-import java.io.*;
-import java.util.*;
-
-import com.github.javaparser.*;
-import com.github.javaparser.ast.*;
-import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.stmt.*;
-
-import net.sourceforge.plantuml.SourceStringReader;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ExpressionStmt;
 
 public class SequenceDiagram {
-    String code;
-    final String inPath;
-   // final String outPath;
-    final String inFuncName;
-    final String inClassName;
+	String code;
+	final String input;
 
-    HashMap<String, String> mapMethodClass;
-    LinkedList<CompilationUnit> linkedList = new LinkedList<CompilationUnit>();
-    
- 
-    HashMap<String, ArrayList<MethodCallExpr>> mapMethodCalls;
+	final String FunctionName;
+	final String ClassName;
 
-    SequenceDiagram(String inPath, String inClassName, String inFuncName) {
-        this.inPath = inPath;
-       // this.outPath = inPath + "/" + outFile + ".png";
-        this.inClassName = inClassName;
-        this.inFuncName = inFuncName;
-        mapMethodClass = new HashMap<String, String>();
-        mapMethodCalls = new HashMap<String, ArrayList<MethodCallExpr>>();
-        code = "@startuml\n";
-    }
+	HashMap<String, String> MethodMap;
+	LinkedList<CompilationUnit> linkedList = new LinkedList<CompilationUnit>();
 
-    public String execute(String input) throws Exception {
-    	
-    	SequenceHelper helper = new SequenceHelper();
-    	linkedList = helper.javaParser(input);
-       // linkedList = getCuArray(inPath);
-    	 for (CompilationUnit cu : linkedList) {
-             String className = "";
-             List<TypeDeclaration> td = cu.getTypes();
-             for (Node n : td) {
-                 ClassOrInterfaceDeclaration coi = (ClassOrInterfaceDeclaration) n;
-                 className = coi.getName();
-                 for (BodyDeclaration bd : ((TypeDeclaration) coi)
-                         .getMembers()) {
-                     if (bd instanceof MethodDeclaration) {
-                         MethodDeclaration md = (MethodDeclaration) bd;
-                         ArrayList<MethodCallExpr> mcea = new ArrayList<MethodCallExpr>();
-                         for (Object bs : md.getChildrenNodes()) {
-                             if (bs instanceof BlockStmt) {
-                                 for (Object es : ((Node) bs)
-                                         .getChildrenNodes()) {
-                                     if (es instanceof ExpressionStmt) {
-                                         if (((ExpressionStmt) (es))
-                                                 .getExpression() instanceof MethodCallExpr) {
-                                             mcea.add(
-                                                     (MethodCallExpr) (((ExpressionStmt) (es))
-                                                             .getExpression()));
-                                         }
-                                     }
-                                 }
-                             }
-                         }
-                         mapMethodCalls.put(md.getName(), mcea);
-                         mapMethodClass.put(md.getName(), className);
-                     }
-                 }
-             }
-         }
-        
-        code += "actor user #black\n";
-        code += "user" + " -> " + inClassName + " : " + inFuncName + "\n";
-        code += "activate " + mapMethodClass.get(inFuncName) + "\n";
-       // helper.convert(inFuncName,code,mapMethodCalls,mapMethodClass);
-        parse(inFuncName);
-        code += "@enduml";
-    
-        System.out.println("Plant UML Code:\n" + code);
-        return code;
-    }
+	HashMap<String, ArrayList<MethodCallExpr>> MethodFlows;
 
-    private void parse(String callerFunc) {
+	SequenceDiagram(final String input, final String ClassName, final String FunctionName) {
+		this.input = input;
 
-        for (MethodCallExpr mce : mapMethodCalls.get(callerFunc)) {
-            String callerClass = mapMethodClass.get(callerFunc);
-            String calleeFunc = mce.getName();
-            String calleeClass = mapMethodClass.get(calleeFunc);
-            if (mapMethodClass.containsKey(calleeFunc)) {
-                code += callerClass + " -> " + calleeClass + " : "
-                        + mce.toStringWithoutComments() + "\n";
-                code += "activate " + calleeClass + "\n";
-                parse(calleeFunc);
-                code += calleeClass + " -->> " + callerClass + "\n";
-                code += "deactivate " + calleeClass + "\n";
-            }
-        }
-       
-    }
+		this.ClassName = ClassName;
+		this.FunctionName = FunctionName;
+		MethodMap = new HashMap<String, String>();
+		MethodFlows = new HashMap<String, ArrayList<MethodCallExpr>>();
+		code = "@startuml\n";
+	}
 
+	public String execute(final String input) throws Exception {
 
+		final SequenceHelper helper = new SequenceHelper();
+		linkedList = helper.javaParser(input);
 
+		for (final CompilationUnit c : linkedList) {
+			String className = "";
+			final List<TypeDeclaration> td = c.getTypes();
+			for (final Node n : td) {
+				final ClassOrInterfaceDeclaration ClassOrInter = (ClassOrInterfaceDeclaration) n;
+				className = ClassOrInter.getName();
+				for (final BodyDeclaration bd : ((TypeDeclaration) ClassOrInter).getMembers()) {
+					if (bd instanceof MethodDeclaration) {
+						final MethodDeclaration md = (MethodDeclaration) bd;
+						final ArrayList<MethodCallExpr> m = new ArrayList<MethodCallExpr>();
+						for (final Object cnode : md.getChildrenNodes()) {
+							if (cnode instanceof BlockStmt) {
+								for (final Object es : ((Node) cnode).getChildrenNodes()) {
+									if (es instanceof ExpressionStmt
+											&& ((ExpressionStmt) es).getExpression() instanceof MethodCallExpr) {
+										m.add((MethodCallExpr) ((ExpressionStmt) es).getExpression());
+									}
+								}
+							}
+						}
+						MethodFlows.put(md.getName(), m);
+						MethodMap.put(md.getName(), className);
+					}
+				}
+			}
+		}
 
+		code = code + "actor user #black\n" + "user" + " -> " + ClassName + " : " + FunctionName + "\n" + "activate "
+				+ MethodMap.get(FunctionName) + "\n";
+
+		convert(FunctionName);
+		code = code + "@enduml";
+
+		System.out.println("Code:\n" + code);
+		return code;
+	}
+
+	private void convert(final String Func1) {
+
+		for (final MethodCallExpr mce : MethodFlows.get(Func1)) {
+			final String Class1 = MethodMap.get(Func1);
+			final String Func2 = mce.getName();
+			final String Class2 = MethodMap.get(Func2);
+			if (MethodMap.containsKey(Func2)) {
+				code = code + Class1 + " -> " + Class2 + " : " + mce.toStringWithoutComments() + "\n"
+						+ "activate " + Class2 + "\n";
+
+				convert(Func2);
+				code = code + Class2 + " -->> " + Class1 + "\n" + "deactivate " + Class2 + "\n";
+
+			}
+		}
+
+	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/SequenceHelper.java
+++ src/SequenceHelper.java
@@ -3,24 +3,18 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.expr.MethodCallExpr;
 
 import net.sourceforge.plantuml.SourceStringReader;
 
 public class SequenceHelper {
-	
+
 	public SequenceHelper() {
 	}
-	
-	
-	
-	
+
 	public LinkedList<CompilationUnit> javaParser(String input) {
 		LinkedList<CompilationUnit> linkedList = new LinkedList<CompilationUnit>();
 
@@ -29,10 +23,10 @@ public class SequenceHelper {
 		File[] files = dir.listFiles();
 		CompilationUnit complilationUnit = null;
 		FileInputStream in = null;
-		
+
 		System.out.println("Input Path: " + input);
 		if (files.length > 0) {
-		
+
 			for (int i = 0; i < files.length; i++) {
 
 				if (files[i].getName().endsWith(".java")) {
@@ -57,19 +51,12 @@ public class SequenceHelper {
 	}
 
 	@SuppressWarnings("deprecation")
-	protected String generateSequenceDiagram(String source,String output) throws IOException {
+	protected String generateSequenceDiagram(String source, String output) throws IOException {
 
-        OutputStream png = new FileOutputStream(output);
-        SourceStringReader reader = new SourceStringReader(source);
-        String desc = reader.generateImage(png);
-        return desc;
+		OutputStream png = new FileOutputStream(output);
+		SourceStringReader reader = new SourceStringReader(source);
+		return reader.generateImage(png);
 
-    }
-	
-	
-	
-
-
-
+	}
 
 }
